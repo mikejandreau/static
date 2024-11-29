f8ab0d8b49c1a535222c5f624fc9d66e2179c741
Mike Jandreau <mikejandreau@gmail.com>
2024-11-29 17:49:45 -0500

    Template updates

--- gulpfile.js
+++ gulpfile.js
@@ -11,11 +11,19 @@ const rename = require('gulp-rename');
 const browsersync = require('browser-sync').create();
 const siteConfig = require('./siteConfig.json'); // Load the JSON configuration file
 
+// JavaScript paths - add or remove JS files as needed
+const scriptSRC = [
+                    "src/js/prism.js", 
+                    "src/js/custom.js"
+                ]; // Path to JS vendor and custom files in desired concat order.
 
+
+// Clean the dist folder
 function clean() {
     return del(['dist']);
 }
 
+// Compile and minify HTML files
 function html() {
     return src('src/html/pages/*.+(html|njk)')
         .pipe(
@@ -36,7 +44,7 @@ function html() {
         .pipe(dest('dist'));
 }
 
-
+// Compile SCSS files and minify CSS
 function scssTask() {
     return src('src/scss/styles.scss', { sourcemaps: true })
         .pipe(sass())
@@ -44,40 +52,44 @@ function scssTask() {
         .pipe(dest('dist/assets', { sourcemaps: '.' }));
 }
 
-
+// Minify JavaScript files
 function jsTask() {
-    return src([
-        'src/js/prism.js', 
-        'src/js/custom.js'
-        ], { sourcemaps: true }) // Target specific files in order
+    return src(scriptSRC, { sourcemaps: true }) // Target specific files in order
         .pipe(concat('scripts.js')) // Combine files into a single file named scripts.js
         .pipe(terser()) // Minify the combined file
         .pipe(dest('dist/assets', { sourcemaps: '.' })); // Output with sourcemaps
 }
 
-exports.jsTask = jsTask;
+// Copy images to the dist/assets folder
+function imgTask() {
+    return src('src/img/**/*') // Adjust the source path as needed
+        .pipe(dest('dist/assets/img')); // Copy to dist/assets/img
+}
 
-
-
+// Serve and watch files for changes
 function browsersyncServe(cb) {
     browsersync.init({
         watch: true,
         server: {
             baseDir: 'dist'
-        }
+        }    
     });
     cb();
 }
 
+// Reload BrowserSync
 function browsersyncReload(cb) {
     browsersync.reload();
     cb();
 }
 
+// Watch for file changes
 function watchFiles() {
-    watch('src/html/**/*', html, browsersyncReload);
+    watch('src/html/**/*', series(html, browsersyncReload)); // Ensure HTML is re-minified
     watch(['src/scss/**/*.scss', 'src/js/**/*.js'], series(scssTask, jsTask, browsersyncReload));
+    watch('src/img/**/*', series(imgTask, browsersyncReload)); // Watch for image changes
 }
 
-exports.build = series(clean, scssTask, jsTask, html);
-exports.default = series(clean, scssTask, jsTask, html, browsersyncServe, watchFiles);
+// Build the project
+exports.build = series(clean, scssTask, jsTask, html, imgTask);
+exports.default = series(clean, scssTask, jsTask, html, imgTask, browsersyncServe, watchFiles);
